bdb7d94d5641d26e90f7e2fb3a2c644e9113d8cf
gitxuzan <gitxuzan@126.com>
2024-07-29 18:33:43 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,13 @@
 /*******************************
 
-脚本名称:测试
-
-*******************************
 [rewrite_local]
-^http[s]?:\/\/newsapi.zankhana.ltd\/yekong_api.+$ url script-response-body https://raw.githubusercontent.com/Xuzan9396/quantumultx_js/main/test.js
+^http[s]?:\/\/uu.tuanyougou.com\/\/video\/registryUser.+$ url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/Ceshi.js
 [mitm] 
-hostname = newsapi.zankhana.ltd
+hostname = *.tuanyougou.*
+
 
 *******************************/
 var obj = JSON.parse($response.body);
-obj.data.ip = "test";
+obj.data.userId = 9999;
+obj.data.endTime = "脚本成功啦";
 $done({body: JSON.stringify(obj)});
