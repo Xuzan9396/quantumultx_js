55d1ec21ce1c3ac8d7ff9a93d1b87f0b3de7e417
gitxuzan <gitxuzan@126.com>
2024-07-29 18:38:28 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
 /*******************************
 
 [rewrite_local]
-^http[s]?:\/\/uu.tuanyougou.com\/\/video\/registryUser.+$ url script-response-body https://raw.githubusercontent.com/Xuzan9396/quantumultx_js/main/test.js
+^http[s]?:\/\/newsapi.zankhana.ltd\/\/video\/registryUser.+$ url script-response-body https://raw.githubusercontent.com/Xuzan9396/quantumultx_js/main/test.js
 [mitm] 
-hostname = *.tuanyougou.*
+hostname = newsapi.zankhana.ltd
 
 
 *******************************/
 var obj = JSON.parse($response.body);
-obj.data.userId = 9999;
-obj.data.endTime = "脚本成功啦";
+obj.data.ip = "test";
 $done({body: JSON.stringify(obj)});
